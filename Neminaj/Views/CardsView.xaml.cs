915fc33833facd37735922bf5acf408ae65c2c07
unknown <sbacharii@gmail.com>
2023-08-06 18:02:57 +0200

    pokrok v kartach, este nedokoncene

--- Neminaj/Views/CardsView.xaml.cs
+++ Neminaj/Views/CardsView.xaml.cs
@@ -24,12 +24,17 @@ public partial class CardsView : ContentPage
 
     ViewContent ViewContent { get; set; } = null;
 
+    Image Image = new Image();
+    Frame Frame = new Frame();
+
     public CardsView(SavedCardRepository savedCardRepository)
     {
         InitializeComponent();
-        
+
         SavedCardRepo = savedCardRepository;
         ViewContent = new ViewContent();
+        this.ViewContent.MainGrid = new Grid();
+        this.ViewContent.GridCards = new Grid();
 
         this.Loaded += async (s, e) => { await BuildPage(); };
     }
@@ -41,7 +46,7 @@ public partial class CardsView : ContentPage
 
         // START Create Main Grid //
 
-        this.ViewContent.MainGrid = new Grid();
+        //this.ViewContent.MainGrid = new Grid();
         this.ViewContent.MainGrid.RowDefinitions.Add(new RowDefinition(GridLength.Star)); // Row 0: Grid for cards
         this.ViewContent.MainGrid.RowDefinitions.Add(new RowDefinition(new GridLength(0.10, GridUnitType.Star))); // Row 1: Buttons Scan and Scan from picture
 
@@ -51,20 +56,28 @@ public partial class CardsView : ContentPage
         // END Create Main Grid //
 
         // START Create Cards Grid //
-        this.ViewContent.GridCards = new Grid();
+        //this.ViewContent.GridCards = new Grid();
 
         // Create columns (fixed 2)
         this.ViewContent.GridCards.ColumnDefinitions.Add(new ColumnDefinition(GridLength.Auto));
         this.ViewContent.GridCards.ColumnDefinitions.Add(new ColumnDefinition(GridLength.Auto));
 
-        // Create rows
-        for (int i = 0; i < this.ViewContent.ListCards.Count; i += 2)
+        // Create rows ( first fixed size of 5)
+        for (int i = 0; i < 10; i += 2)
             this.ViewContent.GridCards.RowDefinitions.Add(new RowDefinition(GridLength.Auto));
+
+        // Check if 5 rows are enough for already saved cards, if not create additional rows
+        if (this.ViewContent.GridCards.RowDefinitions.Count < this.ViewContent.ListCards.Count)
+        {
+            for (int i = 0; i < (this.ViewContent.ListCards.Count - this.ViewContent.GridCards.RowDefinitions.Count); i += 2)
+                this.ViewContent.GridCards.RowDefinitions.Add(new RowDefinition(GridLength.Auto));
+        }
 
         // Create scroll view and add CardsGrid to scroll view
         ScrollView scrollView = new ScrollView();
         scrollView.VerticalScrollBarVisibility = ScrollBarVisibility.Always;
         scrollView.Content = this.ViewContent.GridCards;
+        Grid.SetColumnSpan(scrollView, 2);
         // END Create Cards Grid //
 
         // START Add scroll view with Cards Grid into first Row of Main grid //
@@ -95,28 +108,21 @@ public partial class CardsView : ContentPage
 
         // END Create button Scan from picture //
 
-        // START Fill GridCards //
-
         for (int row = 0, cardCounter = 0; cardCounter < this.ViewContent.ListCards.Count; row++)
         {
             for (int col = 0; ((cardCounter < this.ViewContent.ListCards.Count) && (col < 2)); col++, cardCounter++)
             {
                 var stream = new MemoryStream(this.ViewContent.ListCards[cardCounter].Image);
                 {
-                    Image image = new Image();
-                    image.Source = ImageSource.FromStream(() => stream);
+                    Image.Source = ImageSource.FromStream(() => stream);
+                    Image.Aspect = Aspect.AspectFit;
+                    Image.HeightRequest = (this.Window.Height / 5);
+                    Image.WidthRequest = (this.Window.Width / 2);
 
-                    this.ViewContent.GridCards.Add(image, col, row);
+                    this.ViewContent.GridCards.Add(Image, col, row);
                 }
             }
         }
-
-        foreach (SavedCard card in this.ViewContent.ListCards)
-        {
-
-        }
-
-        // End Fill GridCards //
 
         this.Content = this.ViewContent.MainGrid;
     }
